1e9d84485350328c19a390e33f2bb78b8976c671
treasa <mathithimoe@gmail.com>
2021-08-25 15:13:00 +0630

    design for register and login

--- post.php
+++ post.php
@@ -18,46 +18,6 @@
     <div class="container">
 
         <div class="row">
-            <!-- php code -->
-                    <?php 
-                        if(isset($_GET['p_id'])){
-                            $post_each_id = $_GET['p_id'];
-
-                            $count = '1';
-                            $view_count = 'post_views_count';
-                            $view_sql = "UPDATE posts SET post_views_count = post_views_count + 1 WHERE post_id = :postid";
-                            $view_query = $connection->prepare($view_sql);
-                            $view_query->bindParam(':postid',$post_each_id,PDO::PARAM_INT);
-                            $view_query->execute(); 
-
-                            $sql = "SELECT * FROM posts WHERE post_id = :posteachid";
-                            $query = $connection->prepare($sql);
-                            $query->bindParam(':posteachid',$post_each_id,PDO::PARAM_STR);
-                            $query->execute();
-                            $result = $query->fetchAll(PDO::FETCH_OBJ);
-                            if($query->rowCount()>0){
-                            foreach($result as $row){
-                    ?>
-                    <div class="col-md-12" style="margin-bottom:2%"> 
-                        <div class="card" id="<?php echo $row->post_id; ?>">
-
-                            <img src="img/<?php echo $row->post_img ?>"  alt="image" data-target="postImage" id="get-image" class="img-responsive">
-                            
-                            <div class="card-body">
-                                <h4 class="card-title"><b><?php echo $row->post_title; ?></b></h4>
-                                <p class="card-text" style="text-align:justify" data-target="postDescription"><?php echo $row->post_description; ?></p>
-                                
-                            </div>
-                        </div> 
-                    </div>
-                    <?php 
-                        } } 
-                    } 
-                        else{
-                            header("Location: index.php");
-                        }?>
-            <!--  the end -->
- 
             <!-- Blog Entries Column -->
                <div class="col-md-6">
                   <div class="highlight"><h3 class="recomreq">Recommended Requirements</h3></div> 
