6c924fa1350046613b4908e19f41daba7d8930e8
Krishna <aryalkrishna642@gmail.com>
2021-08-29 23:05:05 +0630

    Configuration of database connection

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,7 +1,7 @@
 <?php 
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','J^JCH6j8Jhk49A9');
+define('DB_PASS','');
 define('DB_NAME','gamehouse');
 
 // define('DB_HOST','localhost');
