fefbdb74d53fcc44488e123a1576aabacf8e43bc
PhonePyae <46389666+PhonePyae@users.noreply.github.com>
2021-10-09 13:27:43 +0630

    Removed gameproject file path in href

--- include/header.php
+++ include/header.php
@@ -18,11 +18,11 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
     <title>Gamehub Myanmar -- <?php echo $activePage; ?></title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/gameproject/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/gameproject/css/front-enddevelop.css">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/front-enddevelop.css">
 
     <!-- Custom CSS -->
-    <link href="/gameproject/css/blog-home.css" rel="stylesheet">
+    <link href="/css/blog-home.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -32,7 +32,7 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
     <![endif]-->    
     
     <!-- Custom Fonts -->
-    <link href="/gameproject/fontawesome-pack/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="/fontawesome-pack/css/all.min.css" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
     <!-- <script src="./js/typeahead/typeahead.bundle.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>
