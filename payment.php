538a9062368a597207ac4b7ff85a4dfb0a21468a
Krishna <aryalkrishna642@gmail.com>
2021-10-25 21:54:30 +0630

    Modified download page with counting the downloaded times

--- payment.php
+++ payment.php
@@ -1,49 +1,70 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include("admin/includes/db.php");
 
 $token = generateRandomString();
 
+if($_POST['user_id'] !='' && $_POST['game_id'] !='' ) {
 
-if(isset($_POST['user_id']) && isset($_POST['game_id']) && isset($_POST['price'])) {
+    $sql = "SELECT * FROM downloads_data WHERE game_id = :game_id";
+    $query = $connection->prepare($sql);
+    $query->bindParam(':game_id',$_POST['game_id'],PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_OBJ);
+    if($query->rowCount() > 0){
+      $count_sql = "UPDATE downloads_data SET count = count + 1 WHERE game_id = :game_id";
+      $count_query = $connection->prepare($count_sql);
+      $count_query->bindParam(':game_id',$_POST['game_id'],PDO::PARAM_INT);
+      $count_query->execute(); 
+    
+    }else{
 
-  $userId = $_POST['user_id'];
-  $gameId = $_POST['game_id'];
-  $price = $_POST['price'];
-  $token = generateRandomString();
-  $userIP = json_encode(IPtoLocation('74.119.146.35'));
+      $userId = $_POST['user_id'];
+      $gameId = $_POST['game_id'];
+      $count = 1;
+      $token = generateRandomString();
+      $userIP = json_encode(IPtoLocation('74.119.146.35'));
 
-  $status = "1";
+      $status = "1";
 
-  // $userIP = $_SERVER['REMOTE_ADDR']."<br/>";
+      // $userIP = $_SERVER['REMOTE_ADDR']."<br/>";
 
-  $sql = "INSERT INTO downloads_data(user_id,
-                                    game_id,
-                                    price,
-                                    token,
-                                    ip_location,
-                                    status) 
-                                    VALUE (
-                                      :userid,
-                                      :gameid,
-                                      :price,
-                                      :token,
-                                      :ip_location,
-                                      :status)";
-            $query = $connection->prepare($sql);
-            $query->bindParam(':userid',$userId,PDO::PARAM_STR);
-            $query->bindParam(':gameid',$gameId,PDO::PARAM_STR);
-            $query->bindParam(':price',$price,PDO::PARAM_STR);
-            $query->bindParam(':token',$token,PDO::PARAM_STR);
-            $query->bindParam(':ip_location',$userIP,PDO::PARAM_STR);
-            $query->bindParam(':status',$status,PDO::PARAM_STR);
+      $sql = "INSERT INTO downloads_data(user_id,
+                                        game_id,
+                                        count,
+                                        token,
+                                        ip_location,
+                                        status) 
+                                        VALUE (
+                                          :userid,
+                                          :gameid,
+                                          :count,
+                                          :token,
+                                          :ip_location,
+                                          :status)";
+                $query = $connection->prepare($sql);
+                $query->bindParam(':userid',$userId,PDO::PARAM_STR);
+                $query->bindParam(':gameid',$gameId,PDO::PARAM_STR);
+                $query->bindParam(':count',$count,PDO::PARAM_STR);
+                $query->bindParam(':token',$token,PDO::PARAM_STR);
+                $query->bindParam(':ip_location',$userIP,PDO::PARAM_STR);
+                $query->bindParam(':status',$status,PDO::PARAM_STR);
 
-            $query->execute();
+                $query->execute();
 
-            $view = "<button name='download' id='buynow' class='btn btn-info btn-lg'>Download</button>";
+                print_r($query->errorInfo());
+          
+    }
 
-            echo $view;
+    require("download.php");
 }
+
+echo $_POST['user_id'];
+echo $_POST['game_id'];
 
  function generateRandomString($length = 10) {
   $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
