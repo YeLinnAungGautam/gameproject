ea99634ccd78357a5dda10d33a74975037a80e2b
Krishna <aryalkrishna642@gmail.com>
2021-10-30 11:37:51 +0630

    Subscription function added

--- include/footer.php
+++ include/footer.php
@@ -7,17 +7,17 @@
             <div class="col-md-4 col-sm-5 col-xs-12 contact-us-box request-box">
               <div class="request-description">
                 <i class="fas fa-info-circle"></i>
-                <p>Have your find your game? If not, request us</p>
+                <h5>Have your find your game? If not, request us</h5>
               </div>
               <div class="request_form">
                 <?php if(isset($_SESSION['user_id'])) { ?>
 
-                <form action="">
+                <!-- <form action="">
                   <div class="form-group">  
                     <input type="text" class="form-control" id="game_request" name="game_request" placeholder="Enter game title">
                   </div>
                   <button type="submit" class="btn btn-default">Request</button>
-                </form>
+                </form> -->
                 
                 <?php } ?>
               </div>
@@ -46,18 +46,18 @@
                         Keep in Touch with Gamehub Myanmar
                       </h3>
                     </div>
+
                     <div class="footer-des">
-                      <p>Get new games notifications</p>
+                      <p id="sub-alert-success" style="display:none;color:green;">Thank you for your subscription.</p>
+                      <p id="sub-alert-fail" style="display:none;color:yellow;">You have already subscribed.</p>
                     </div>
+
                     <div class="subscribe_form mar-topper">
-                      <form action="subscribe.php">
-                        <div class="form-group">
-                          <input type="email" class="form-control" id="news_subscribe" name="news_subscribe" placeholder="Enter your email">
-                        </div>
-                        <button type="submit" class="btn btn-default">Subscribe</button>
+                      <form id="sub-form" action="">
+                        <input type="email" class="form-control" id="sub_email" required name="news_subscribe" placeholder="Enter your email">
+                        <input type="submit" id="sub-btn" class="btn btn-default" value="Subscribe">
                       </form>
                     </div>
-                    
                 </div> 
               </div>
             </div>
@@ -65,20 +65,20 @@
 
     <div class="copyright container-fluid mar-topper">
       <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-sm-3 col-xs-3 social-ft">
-                  <div class="soical-links">
-                      <a href="#"><i class="fab fa-facebook-f"></i></a>
-                      <a href="#"><i class="fab fa-instagram"></i></a>
-                      <a href="#"><i class="fab fa-twitter"></i></a>
-                  </div>
+          <div class="row">
+              <div class="col-md-6 col-sm-3 col-xs-3 social-ft">
+                <div class="soical-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
                 </div>
-                <div class="col-md-6 col-sm-9 col-xs-9 text-right copy-ft">
-                    <span style="line-height: 2;">Copyright &copy; <?php echo date("Y")?>. All Rights Reserved by <a href="index">Gamehub Myanmar</a>. </span>
-                </div>
+              </div>
+              <div class="col-md-6 col-sm-9 col-xs-9 text-right copy-ft">
+                  <span style="line-height: 2;">Copyright &copy; <?php echo date("Y")?>. All Rights Reserved by <a href="index">Gamehub Myanmar</a>. </span>
+              </div>
 
-            </div>
           </div>
+        </div>
             <!-- /.row -->
       </div>   
     </div>
@@ -86,7 +86,7 @@
 </footer>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php echo $baseurl;?>/js/bootstrap.min.js"></script>
 
     <!-- Splide Js  -->
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
@@ -102,59 +102,51 @@
 
       $(document).ready(function() {
 
-         var p_user_id = $("#p_user_id").val();
-  
-  //modal
-            const modal = $(".modal");
-            const modalToggle = $(".modal-toggle");
-            const modalWrapper = $(".modal-wrapper");
-            const modalHeight = modalWrapper.height();
-
-            //countdown
-            function coundDown() {
-              console.log("countdown");
-              var countdownNumberEl = document.getElementById("countdown-number");
-              var countdown = 5;
-              countdownNumberEl.textContent = countdown;
-              var downloadTimer = setInterval(function() {
-                countdown--; // 5 to 0
-                countdownNumberEl.textContent = countdown;
-
-                if (countdown > 0) {
-                  modalToggle.unbind("click", showMConfirmModal);
-                } else {
-                  modalToggle.bind("click", showMConfirmModal);
-                  clearInterval(downloadTimer);
-                  modal.removeClass("is-visible");
+        $('#ptsearch').typeahead({
+            source: function(query, result){
+              $.ajax({
+                url: base_url+"/fetch.php",
+                method: "POST",
+                data: {query:query},
+                dataType:"json",
+                success:function(data){
+                  result($.map(data, function(item){
+                    return item;
+                  }));
                 }
-              }, 1000);
-            }
-
-
-            //show modal
-            function showMConfirmModal(e) {
-              console.log('icon click');
-              const modalPosY = $(document).outerHeight() / 2 - modalHeight/2;
-              modalWrapper.css("margin-top", modalPosY)
-              //fire countedown modal
-              if ($("#count-loading").length) {
-                e.preventDefault();
-                modal.addClass('is-visible');
-                coundDown();
-              }else{
-                //fire confirm modal
-                e.preventDefault();
-                modal.toggleClass('is-visible');
-              }
-            }
-
-            modalToggle.click(showMConfirmModal);
-
-              $(".btn-cancel").click(function(e) {
-                modal.removeClass("is-visible");
-              });
+              })
+            } 
+          });
       });
 
+       $("#sub-form").submit(function(e) {
+          var email = $("#sub_email").val();
+          $.ajax({
+            type: "POST",
+            url: base_url+"/subscription.php",
+            data: {email: email},
+            beforeSend: function() {
+              $("#overlay").css({ 'display' : 'block'})
+            },
+            success: function(response)
+            {
+              $("#overlay").css({ 'display' : 'none'})
+              if(response == "1"){
+                $('#sub-alert-fail').fadeIn("fast", function(){        
+                  $("#sub-alert-fail").fadeOut(2000);
+                });
+              }else{
+                $('#sub-alert-success').fadeIn("fast", function(){        
+                  $("#sub-alert-success").fadeOut(2000);
+                });
+              }
+            }
+          
+        });
+
+        return false;
+
+      });
 
        $("#buynow").on('click',function() {
           var userId = $("#user_id").val();
@@ -164,13 +156,8 @@
           var count = $('#d_count').val();
           if(userId == '' || gameId == '') {
             window.sessionStorage.setItem("gameId",gameId); 
-            // location.href = "login.php?action=cfs";
             location.href = base_url+"/login"
           }else{
-            // $("#paymentModal").modal({
-            //   backdrop: 'static',
-            //   keyboard: false
-            // })
             if(count < 20) {
             $.ajax({
             type: "POST",
@@ -193,31 +180,12 @@
           }
        })
 
-      $(document).ready(function(){
-          $('#ptsearch').typeahead({
-            source: function(query, result){
-              $.ajax({
-                url: "fetch.php",
-                method: "POST",
-                data: {query:query},
-                dataType:"json",
-                success:function(data){
-                  result($.map(data, function(item){
-                    return item;
-                  }));
-                }
-              })
-            } 
-          });
-        });
-
         new Splide( '.splide', {
             type  : 'fade',
             rewind: true,   
             autoplay: true,
           } ).mount();
     
-
         document.addEventListener("DOMContentLoaded", function () {
           slider_one();
           slider_two();
@@ -273,14 +241,11 @@
                     window.location.assign(resp.result.webContentLink);
                 } else {
                     var formatted = JSON.stringify(resp.data.result, null, 2);
-                    console.log(formatted);
                 }
             })
         })
         };
         // 1. Load the JavaScript client library.
-
-        
     </script>
 </body>
 
